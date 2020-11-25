ac4de6379851f1d1ebc11c5d4edff0029d679b3a
janice880624 <47550853+janice880624@users.noreply.github.com>
2020-11-25 22:54:16 +0800

    1125

--- house/house.js
+++ house/house.js
@@ -63,7 +63,6 @@ function buzzer_music(m) {
   return musicNotes;
 }
 
-
 boardReady({board: 'Smart', device: device_id, transport: 'mqtt'}, async function (board) {
   board.samplingInterval = 50;
   lcd1602 = getLCD1602(board,4,5,0x27);
