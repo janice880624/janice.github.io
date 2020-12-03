23d52cde6443fe37958faa55e8fcd77449070fbd
janice880624 <47550853+janice880624@users.noreply.github.com>
2020-12-03 22:02:36 +0800

    led

---
+++ xmastree/xmastree_led.js
@@ -0,0 +1,28 @@
+var loc=location.href;
+var n1=loc.length; //地址的總長度
+var n2=loc.indexOf("="); //取得=號的位置
+var device_id = decodeURI(loc.substr(n2+1, n1-n2));//從=號後面的内容
+alert("Device ID:" + device_id); 
+// document.write("device_id:"+device_id)
+console.log('=> ' + device_id);
+
+var dfplayer;
+var led;
+var photocell;
+var light;
+var myData;
+
+
+boardReady({board: 'Smart', device: device_id, transport: 'mqtt'}, function (board) {
+  board.samplingInterval = 50;
+  led = getLed(board, 5);
+  document.getElementById('btn1').addEventListener('click',function () {
+    led.on();
+  });
+  document.getElementById('btn2').addEventListener('click',function () {
+    led.off();
+  });
+  document.getElementById('btn3').addEventListener('click',function () {
+    led.blink(500);
+  });
+});
