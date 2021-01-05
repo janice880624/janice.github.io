4df260714ee53ae4f4518821ac2c49a5a71edffe
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-01-05 23:51:03 +0800

    line_blockly

---
+++ blockly_line/blockly/javascript.js
@@ -0,0 +1,10 @@
+// from generator stub js
+
+Blockly.JavaScript['line'] = function(block) {
+    var value_channelid = Blockly.JavaScript.valueToCode(block, 'channelId', Blockly.JavaScript.ORDER_ATOMIC);
+    var value_channelsecret = Blockly.JavaScript.valueToCode(block, 'channelSecret', Blockly.JavaScript.ORDER_ATOMIC);
+    var value_token = Blockly.JavaScript.valueToCode(block, 'Token', Blockly.JavaScript.ORDER_ATOMIC);
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return code;
+};
---
+++ blockly_line/main.js
@@ -0,0 +1,14 @@
++(function (window, document) {
+    'use strict';
+    function line(v1, v2, v3) {
+    var obj = {};
+    if (v1 != "''" || v1.length > 0) {
+    obj.value1 = v1;
+    }
+    if (v2 != "''" || v1.length > 0) {
+        obj.value2 = v2;
+    }
+    if (v3 != "''" || v1.length > 0) {
+    obj.value3 = v3;
+    }
+}(window, window.document));
