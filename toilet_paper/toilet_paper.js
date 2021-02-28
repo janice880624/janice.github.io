508f2fff38f91967495e5e27f1d2180070f51968
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-03-01 00:37:59 +0800

    0301

--- toilet_paper/toilet_paper.js
+++ toilet_paper/toilet_paper.js
@@ -5,24 +5,45 @@ var ul_echo = 16;
 
 boardReady({board1: 'Smart', device: '10ywPWNV', transport: 'mqtt'}, function (board1) {
     board1.samplingInterval = 50;
-    ultrasonic = getUltrasonic(board1, ul_echo, ul_trig);
-    ultrasonic.ping(function (cm) {
-        document.getElementById('sensor_1').innerHTML = ultrasonic.distance;
+    ultrasonic1 = getUltrasonic(board1, ul_echo, ul_trig);
+    ultrasonic1.ping(function (cm) {
+        document.getElementById('sensor_1').innerHTML = ultrasonic1.distance;
+        if (ultrasonic1.distance > 20) {
+            document.getElementById('sensor_1_add').innerHTML = '補充';
+            document.getElementById('box1').style.backgroundColor =  "rgb(221, 156, 119)";
+        } else{
+            document.getElementById('sensor_1_add').innerHTML = ' ';
+            document.getElementById('box1').style.backgroundColor =  "rgb(169, 240, 240)";
+        }
     }, 500);
 });
 
 boardReady({board2: 'Smart', device: '10dLYwYy', transport: 'mqtt'}, function (board2) {
     board2.samplingInterval = 50;
-    ultrasonic = getUltrasonic(board2, ul_echo, ul_trig);
-    ultrasonic.ping(function (cm) {
-        document.getElementById('sensor_2').innerHTML = ultrasonic.distance;
+    ultrasonic2 = getUltrasonic(board2, ul_echo, ul_trig);
+    ultrasonic2.ping(function (cm) {
+        document.getElementById('sensor_2').innerHTML = ultrasonic2.distance;
+        if (ultrasonic2.distance > 20) {
+            document.getElementById('sensor_2_add').innerHTML = '補充';
+            document.getElementById('box2').style.backgroundColor =  "rgb(221, 156, 119)";
+        } else{
+            document.getElementById('sensor_2_add').innerHTML = ' ';
+            document.getElementById('box2').style.backgroundColor =  "rgb(169, 240, 240)";
+        }
     }, 500);
 });
 
 boardReady({board3: 'Smart', device: '10Vk7gDV', transport: 'mqtt'}, function (board3) {
     board3.samplingInterval = 50;
-    ultrasonic = getUltrasonic(board3, ul_echo, ul_trig);
-    ultrasonic.ping(function (cm) {
-        document.getElementById('sensor_3').innerHTML = ultrasonic.distance;
+    ultrasonic3 = getUltrasonic(board3, ul_echo, ul_trig);
+    ultrasonic3.ping(function (cm) {
+        document.getElementById('sensor_3').innerHTML = ultrasonic3.distance;
+        if (ultrasonic3.distance > 20) {
+            document.getElementById('sensor_3_add').innerHTML = '補充';
+            document.getElementById('box3').style.backgroundColor =  "rgb(221, 156, 119)";
+        } else{
+            document.getElementById('sensor_3_add').innerHTML = ' ';
+            document.getElementById('box3').style.backgroundColor =  "rgb(169, 240, 240)";
+        }
     }, 500);
 });
