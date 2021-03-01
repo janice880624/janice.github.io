ef922f34b833b4e354325808c22a8d93f6e1219b
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-03-01 11:05:51 +0800

    notify

--- toilet_paper/toilet_paper.js
+++ toilet_paper/toilet_paper.js
@@ -1,7 +1,12 @@
-var ultrasonic;
+var ultrasonic1;
+var ultrasonic2;
+var ultrasonic3;
 
 var ul_trig = 14;
 var ul_echo = 16;
+
+var who = 0;
+var send = false;
 
 boardReady({board1: 'Smart', device: '10ywPWNV', transport: 'mqtt'}, function (board1) {
     board1.samplingInterval = 50;
@@ -11,7 +16,8 @@ boardReady({board1: 'Smart', device: '10ywPWNV', transport: 'mqtt'}, function (b
         if (ultrasonic1.distance > 20) {
             document.getElementById('sensor_1_add').innerHTML = '補充';
             document.getElementById('box1').style.backgroundColor =  "rgb(221, 156, 119)";
-        } else{
+            who = 1;
+        } else{;
             document.getElementById('sensor_1_add').innerHTML = ' ';
             document.getElementById('box1').style.backgroundColor =  "rgb(169, 240, 240)";
         }
@@ -26,6 +32,7 @@ boardReady({board2: 'Smart', device: '10dLYwYy', transport: 'mqtt'}, function (b
         if (ultrasonic2.distance > 20) {
             document.getElementById('sensor_2_add').innerHTML = '補充';
             document.getElementById('box2').style.backgroundColor =  "rgb(221, 156, 119)";
+            who = 2;
         } else{
             document.getElementById('sensor_2_add').innerHTML = ' ';
             document.getElementById('box2').style.backgroundColor =  "rgb(169, 240, 240)";
@@ -41,9 +48,45 @@ boardReady({board3: 'Smart', device: '10Vk7gDV', transport: 'mqtt'}, function (b
         if (ultrasonic3.distance > 20) {
             document.getElementById('sensor_3_add').innerHTML = '補充';
             document.getElementById('box3').style.backgroundColor =  "rgb(221, 156, 119)";
+            who = 3;
         } else{
             document.getElementById('sensor_3_add').innerHTML = ' ';
             document.getElementById('box3').style.backgroundColor =  "rgb(169, 240, 240)";
         }
     }, 500);
 });
+
+const lineNotify = require('line-notify-nodejs')('XEyxMI5MOWSQRqZLUBg73R5MLHJXxkMOY3nMOFahDBy');
+
+lineNotify.notify({
+   message: '智慧監控裝置已連線',
+}).then(() => {
+   console.log('send completed!');
+});
+
+setInterval(function () {
+    if (who == 1 && send == false) {
+        lineNotify.notify({
+            message: '一號衛生紙要補充了喔!!',
+        }).then(() => {
+            console.log('send completed!');
+        });
+        send = true;
+    } else if (who == 2 && send == false) {
+        lineNotify.notify({
+            message: '二號衛生紙要補充了喔!!',
+        }).then(() => {
+            console.log('send completed!');
+        });
+        send = true;
+    } else if (who == 3 && send == false) {
+        lineNotify.notify({
+            message: '三號衛生紙要補充了喔!!',
+        }).then(() => {
+            console.log('send completed!');
+        });
+        send = true;
+    }
+}, 1000 * 5);
+
+// XEyxMI5MOWSQRqZLUBg73R5MLHJXxkMOY3nMOFahDBy
