84c27300dd1d5d6ea7828bc0eb430ee378e8ed05
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-03-05 23:35:28 +0800

    rep

--- weather/weather.js
+++ weather/weather.js
@@ -6,6 +6,18 @@ var s=NowDate.getSeconds();
 document.write("現在是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日 ");
 document.write( h+' 時 '+m+' 分 '+s+' 秒 ')
 
-function reP(){
-    document.getElementById('climbing').style.display = "block";
+function reP1(){
+    document.getElementById('climbing1').style.display = "block";
+}  
+function reP2(){
+    document.getElementById('climbing2').style.display = "block";
+}  
+function reP3(){
+    document.getElementById('climbing3').style.display = "block";
+}  
+function reP4(){
+    document.getElementById('climbing4').style.display = "block";
+}  
+function reP5(){
+    document.getElementById('climbing5').style.display = "block";
 }  
